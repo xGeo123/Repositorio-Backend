d9ea01ed53306faee99c0d2d0423dc1a6c3c6057
Hugo Emmanuel Angulo Castillo <142077367+EmmaNuGL@users.noreply.github.com>
2026-04-27 14:48:39 -0500

    clases

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import Server from './server/server.js';
+import colors from 'colors';
+import dotenv from 'dotenv';
+dotenv.config();
 const server = new Server();
 server.listen();
--- server/server.js
+++ server/server.js
@@ -1,23 +1,31 @@
 import express from 'express';
 import cors from 'cors'; 
 import indexRoutes from '../routes/index.routes.js';
+import * as db from '../db/cnn_mongodb.js';
 export default class Server {
     constructor() {
         this.app = express();
-        this.port =3000;
+        this.port =process.env.PORT || 3000;
         this.generalRoute= '/api/';
         this.middlewares();
         this.routes();
     }
+     async conectarAMongoDB(){
+            if(!db.isConected){
+                await db.conectarAMongoDB();
+            }
+        }
     middlewares() { 
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.static('public'));
     }
     routes() {
+       
      // localhost:3000/api/ejemplo
     this.app.use(this.generalRoute, indexRoutes);
 }
+
 listen() {
     this.app.listen(this.port, () => {
         console.log(`Servidor corriendo en puerto ${this.port}`);
